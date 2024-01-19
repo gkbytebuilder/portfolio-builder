23b927b2abd0ffb5321a4b1e7402962d1e39e26d
gideon.govind <gideon.govind@rubicotech.in>
2024-01-19 17:22:55 +0530

    feat: :sparkles: template for contact, resume and work

---
+++ src/components/ResumeComponent.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="container mx-auto w-full lg:rounded-2xl bg-white">
+    <div data-aos="fade" class="aos-init aos-animate">
+      <div class="container mx-auto w-full sm:px-5 md:px-10 lg:px-20">
+        <div class="py-12 px-4">
+          <h2 class="after-effect after:left-44 mb-[40px] mt-12 lg:mt-0">Resume</h2>
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-6">
+            <div>
+              <div class="flex items-center space-x-2 mb-4">
+                <div class="text-6xl text-[#F95054]">
+                  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M20 7h-4V5l-2-2h-4L8 5v2H4c-1.1 0-2 .9-2 2v5c0 .75.4 1.38 1 1.73V19c0 1.11.89 2 2 2h14c1.11 0 2-.89 2-2v-3.28c.59-.35 1-.99 1-1.72V9c0-1.1-.9-2-2-2zM10 5h4v2h-4V5zM4 9h16v5h-5v-3H9v3H4V9zm9 6h-2v-2h2v2zm6 4H5v-3h4v1h6v-1h4v3z"></path></svg>
+                </div>
+                <h4 class="text-5xl font-medium">Education</h4>
+              </div>
+              <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg" style="background: rgb(255, 244, 244);">
+                <span class="text-tiny text-gray-lite">2021 - Present</span>
+                <h3 class="text-xl"> Uttarakhand Technical University  </h3>
+                <p class="">Master of Computer Application</p>
+              </div>
+              <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg" style="background: rgb(255, 241, 251);">
+                <span class="text-tiny text-gray-lite ">2019 - 2021</span>
+                <h3 class="text-xl"> Hemwati Nandan Bahuguna Garhwal University </h3>
+                <p class="">Bachelor of Science  - PCM</p>
+              </div>
+              <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg" style="background: rgb(255, 244, 244);">
+                <span class="text-tiny text-gray-lite">2017 - 2019</span>
+                <h3 class="text-xl"> Infotechus Pvt. Ltd.  </h3>
+                <p class="">Associate Software Engineer</p>
+              </div>
+            </div>
+            <div>
+              <div class="flex items-center space-x-2 mb-4">
+                <div class="text-6xl text-[#F95054]">
+                  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72L12 15l5-2.73v3.72z"></path></svg>
+                </div>
+                <h4 class="text-5xl font-medium">Experience</h4>
+              </div>
+              <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg" style="background: rgb(255, 244, 244);">
+                <span class="text-tiny text-gray-lite">2021 - Present</span>
+                <h3 class="text-xl"> Rubico It Private Limited  </h3>
+                <p class="">Software Engineer</p>
+              </div>
+              <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg" style="background: rgb(255, 241, 251);">
+                <span class="text-tiny text-gray-lite ">2019 - 2021</span>
+                <h3 class="text-xl"> Biz2Credit </h3>
+                <p class="">Software Engineer</p>
+              </div>
+              <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg" style="background: rgb(255, 244, 244);">
+                <span class="text-tiny text-gray-lite">2017 - 2019</span>
+                <h3 class="text-xl"> Infotechus Pvt. Ltd.  </h3>
+                <p class="">Associate Software Engineer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container bg-color-810 py-12 px-4 sm:px-5 md:px-10 lg:px-20">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div class="col-span-1">
+            <h4 class="text-5xl font-medium mb-6">Working Skills</h4>
+            <div class="mb-7">
+                <div class="flex justify-between py-1"><span class=" text-base text-gray-lite font-semibold">Backend Development</span><span class=" text-base font-semibold text-gray-lite pr-5">80%</span></div>
+                <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                  <path class="rc-progress-line-trail" d="M 0.5,0.5
+                L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                  <path class="rc-progress-line-path" d="M 0.5,0.5
+                L 99.5,0.5" stroke-linecap="round" stroke="#FF6464" stroke-width="1" fill-opacity="0" style="stroke-dasharray: 79.2px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;"></path>
+                </svg>
+            </div>
+            <div class=" mb-7">
+                <div class="flex justify-between py-1"><span class=" text-base text-gray-lite font-semibold">Database Design and Development </span><span class=" text-base font-semibold text-gray-lite pr-5">95%</span></div>
+                <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                  <path class="rc-progress-line-trail" d="M 0.5,0.5
+                L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                  <path class="rc-progress-line-path" d="M 0.5,0.5
+                L 99.5,0.5" stroke-linecap="round" stroke="#9272D4" stroke-width="1" fill-opacity="0" style="stroke-dasharray: 94.05px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;"></path>
+                </svg>
+            </div>
+            <div class=" mb-7">
+                <div class="flex justify-between py-1"><span class=" text-base text-gray-lite font-semibold">Frontend Development</span><span class=" text-base font-semibold text-gray-lite pr-5">75%</span></div>
+                <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                  <path class="rc-progress-line-trail" d="M 0.5,0.5
+              L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                  <path class="rc-progress-line-path" d="M 0.5,0.5
+              L 99.5,0.5" stroke-linecap="round" stroke="#CA56F2" stroke-width="1" fill-opacity="0" style="stroke-dasharray: 74.25px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;"></path>
+                </svg>
+            </div>
+            <div class=" mb-7">
+                <div class="flex justify-between py-1"><span class=" text-base text-gray-lite font-semibold">Change Data Capture/Search/API</span><span class=" text-base font-semibold text-gray-lite pr-5">65%</span></div>
+                <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                  <path class="rc-progress-line-trail" d="M 0.5,0.5
+                L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                  <path class="rc-progress-line-path" d="M 0.5,0.5
+                L 99.5,0.5" stroke-linecap="round" stroke="#5185D4" stroke-width="1" fill-opacity="0" style="stroke-dasharray: 64.35px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;"></path>
+                </svg>
+            </div>
+          </div>
+          <div class="col-span-1">
+            <h4 class="text-5xl font-medium mb-8">Knowledge's</h4>
+            <div class="flex gap-x-3 gap-y-3 md:gap-y-3 md:gap-x-3 flex-wrap">
+              <button class="resume-btn text-[15px]">PHP/Laravel</button>
+              <button class="resume-btn text-[15px]">Javascript</button>
+              <button class="resume-btn text-[15px]">NodeJS</button>
+              <button class="resume-btn text-[15px]">VueJS</button>
+              <button class="resume-btn text-[15px]">MYSQL</button>
+              <button class="resume-btn text-[15px]">MONGO</button>
+              <button class="resume-btn text-[15px]">ElasticSearch</button>
+              <button class="resume-btn text-[15px]">Monstache</button>
+              <button class="resume-btn text-[15px]">Golang</button>
+              <button class="resume-btn text-[15px]">Time Management</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="overflow-hidden rounded-b-2xl bg-slate-50">
+        <div class="container">
+          <p class="text-center py-6 text-gray-lite dark:text-color-910">
+            © 2024 All Rights Reserved
+          </p>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResumeComponent',
+}
+</script>
+
+<style scoped>
+.after-effect {
+  position: relative;
+  display: inline-block;
+  font-size: 2.5rem;
+  font-weight: 700;
+}
+
+.after-effect::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to right, #fa5252, #dd2476);
+  opacity: 0; /* Initially, make it transparent */
+  transition: opacity 0.3s ease-in-out; /* Add a transition for a smooth effect */
+}
+
+.after-effect:hover::after {
+  opacity: 1; /* On hover, make the pseudo-element visible */
+}
+
+.text-5xl {
+    font-size: 1.625rem;
+}
+
+.resume-btn {
+  cursor: default;
+  border-radius: 0.5rem;
+  background-color: rgb(237 242 242/var(--tw-bg-opacity));
+  padding: 0.5rem 1.25rem;
+  color: rgb(68 86 108/var(--tw-text-opacity));
+}
+/* Add your Tailwind CSS styles here */
+</style>
